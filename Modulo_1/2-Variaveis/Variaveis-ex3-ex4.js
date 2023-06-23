f5bb77a6ca7122bb3a20e408e1cb2581b5d36858
joaolotjr <joaolotjr@gmail.com>
2023-06-23 17:50:12 -0300

    Commit exercise 3 and 4 - Variables

---
+++ Modulo_1/2-Variaveis/Variaveis-ex3-ex4.js
@@ -0,0 +1,13 @@
+let x = 10, y = 2;
+
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}.`);
+
+resultado = x - y;
+console.log(`A subtração de ${x} com ${y} é ${resultado}.`);
+
+resultado = x * y;
+console.log(`A multiplicação de ${x} com ${y} é ${resultado}.`);
+
+resultado = x / y;
+console.log(`A Divisão de ${x} com ${y} é ${resultado}.`);
