2aeb91289d4dc2f007e0192ca62ae9a896e30614
joaolotjr <joaolotjr@gmail.com>
2023-06-24 16:30:44 -0300

    Commit Exercise 11

---
+++ Modulo_1/2-Variaveis/Exercicios/11/index.js
@@ -0,0 +1,8 @@
+let pi = Math.PI, r = 3, d = 2 * r;
+
+let v = (4 / 3) * pi *(r ** 3);
+
+let vPI = (4 / 3) * (r ** 3); //sem PI na eq.
+
+console.log(`O volume de uma esfera de raio ${r} é ${vPI.toFixed(1)} PI`)
+
