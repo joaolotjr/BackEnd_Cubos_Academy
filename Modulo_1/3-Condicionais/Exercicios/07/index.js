3108f8394ae493fa9d4b0efa4a72d3ee14c49030
joaolotjr <joaolotjr@gmail.com>
2023-06-27 20:10:39 -0300

    Commit Exercise 7

--- Modulo_1/3-Condicionais/Exercicios/07/index.js
+++ Modulo_1/3-Condicionais/Exercicios/07/index.js
@@ -3,3 +3,10 @@ const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+
+
+if(aposentada || portadoraDeDoenca){console.log(`ISENTA`)}
+
+else if (totalDeRendimentos <= 28_559_70){console.log(`VAZA LEAO! JA TA DIFICIL SEM VOCE`)}
+
+else {console.log(`PEGA LEAO`)}
