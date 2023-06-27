1350a1c23989e3070a1f7983f34e38c488684fc3
joaolotjr <joaolotjr@gmail.com>
2023-06-27 18:26:49 -0300

    Commit Exercise 1

--- Modulo_1/3-Condicionais/Exercicios/01/index.js
+++ Modulo_1/3-Condicionais/Exercicios/01/index.js
@@ -1,4 +1,23 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "pedra";
+const jogada2 = "papel";
 
 //seu código aqui
+if (jogada1 === jogada2) {
+    console.log(`Empate`);
+    }
+else if ((jogada1 == "pedra" && jogada2 == "tesoura" ) || (jogada2 == "pedra" && jogada1 == "tesoura" )){
+
+    console.log(`Pedra`);
+}
+
+else if ((jogada1 == "papel" && jogada2 == "pedra" ) || (jogada2 == "papel" && jogada1 == "pedra" )){
+
+    console.log(`papel`);
+}
+
+else if ((jogada1 == "tesoura" && jogada2 == "papel" ) || (jogada2 == "tesoura" && jogada1 == "papel" )){
+
+    console.log(`tesoura`);
+}
+
+
