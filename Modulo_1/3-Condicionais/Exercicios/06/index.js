0606c3ebf1cb92c42deadca7aaacc95799b2ac0d
joaolotjr <joaolotjr@gmail.com>
2023-06-27 19:59:01 -0300

    Commit Exercise 6

--- Modulo_1/3-Condicionais/Exercicios/06/index.js
+++ Modulo_1/3-Condicionais/Exercicios/06/index.js
@@ -1,3 +1,12 @@
 const alturaEmCm = 185;
 
 //seu código aqui
+
+if (alturaEmCm < 180) console.log(`REPROVADO`);
+
+else if(alturaEmCm >= 180 && alturaEmCm <= 185) console.log(`LÍBERO`);
+else if(alturaEmCm >= 186 && alturaEmCm <= 195) console.log(`PONTEIRO`);
+else if(alturaEmCm >= 196 && alturaEmCm <= 205) console.log(`OPOSTO`);
+else if(alturaEmCm > 205 ) console.log(`CENTRAL`);
+
+else console.log(`ERRO`)
