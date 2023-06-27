4bad65fa6ab533b07adcf670f59a0d05e938331d
joaolotjr <joaolotjr@gmail.com>
2023-06-27 19:31:10 -0300

    Commit Exercise 4

---
+++ Modulo_1/3-Condicionais/Exercicios/04/index.js
@@ -0,0 +1,14 @@
+const ladoA = 3;
+const ladoB = 3;
+
+if(ladoA === ladoB){
+
+    if(ladoA == 0) console.log(`Bucha de Branco`);
+    if(ladoA == 1) console.log(`Bucha de As`);
+    if(ladoA == 2) console.log(`Bucha de Duque`);
+    if(ladoA == 3) console.log(`Bucha de Terno`);
+    if(ladoA == 4) console.log(`Bucha de Quadra`);
+    if(ladoA == 5) console.log(`Bucha de Quina`);
+    if(ladoA == 6) console.log(`Bucha de Sena`);
+}
+else console.log(`NAO`)
