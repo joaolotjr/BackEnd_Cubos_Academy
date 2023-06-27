8bcf1fbae533a574e571debd86501f03def3d173
joaolotjr <joaolotjr@gmail.com>
2023-06-27 20:45:06 -0300

    Commit Exercise 9

--- Modulo_1/3-Condicionais/Exercicios/09/index.js
+++ Modulo_1/3-Condicionais/Exercicios/09/index.js
@@ -1,1 +1,10 @@
 const nota = 8.5;
+
+
+if( nota >= 9 && nota <= 10 ) console.log(`O estudando obteve conceito A`);
+else if( nota >= 8 && nota <= 8.9) console.log(`O estudando obteve conceito B`);
+else if( nota >= 6 && nota <= 7.9) console.log(`O estudando obteve conceito C`);
+else if( nota >= 4 && nota <= 5.9) console.log(`O estudando obteve conceito D`);
+else if( nota < 4 ) console.log(`O estudando obteve conceito E`);
+else console.log(`ERRO`);
+
