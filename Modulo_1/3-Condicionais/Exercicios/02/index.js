d34db856742463a385d2e2890cb0d35838ee2f6e
joaolotjr <joaolotjr@gmail.com>
2023-06-27 19:18:45 -0300

    Commit Exercise 2

--- Modulo_1/3-Condicionais/Exercicios/02/index.js
+++ Modulo_1/3-Condicionais/Exercicios/02/index.js
@@ -1,4 +1,13 @@
-const jogada1 = 5;
+const jogada1 = 6;
 const jogada2 = 3;
 
 //seu código aqui
+
+let total = jogada1 + jogada2;
+
+if ((total % 2) == 0) {
+    console.log(`par`)
+}
+else console.log(`Impar`)
+
+
