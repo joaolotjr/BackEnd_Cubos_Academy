0127a0ca49938f08cb1d8d47da1065f0f9b9d1c1
joaolotjr <joaolotjr@gmail.com>
2023-06-27 22:12:31 -0300

    Commit Exercise 16

--- Modulo_1/3-Condicionais/Exercicios/16/index.js
+++ Modulo_1/3-Condicionais/Exercicios/16/index.js
@@ -2,4 +2,8 @@
 const idadeDoAluno = 16;
 
 //valor verdadeiro ou falso que informa se possui responsável
-const possuiResponsavel = false;
+const possuiResponsavel = true;
+
+if(idadeDoAluno >=18 || possuiResponsavel) console.log(`Rematrícula realizada com sucesso`);
+
+else if(idadeDoAluno < 18) console.log(`Não é possível fazer a rematrícula`);
