8977c01130fa47a28fb28aaf38c2a607d5337ab3
joaolotjr <joaolotjr@gmail.com>
2023-06-27 22:37:26 -0300

    Commit Exercise 17

--- Modulo_1/3-Condicionais/Exercicios/17/index.js
+++ Modulo_1/3-Condicionais/Exercicios/17/index.js
@@ -1,8 +1,33 @@
 //valor do produto comprado.
-const valorDoProduto = 100000;
+const valorDoProduto = 1000_00;
 
 //quantidade de parcelas
 const quantidadeDoParcelamento = 10;
 
 //valor pago
-const valorPago = 300;
+const valorPago = 300_00;
+
+// OBS: Melhorar esse anunciado pq ta bem ruim, misturando centavos com real
+// Esse valorPago, foi feito em pagamento a vista pela fernanda? Ou ja são as parcelas pagas?
+// São coisas que mudam a lógica toda.
+
+//Com isso fiz a quantidade de parcelas faltante, contando que ela paga a mesma parcela desde o começo.
+
+ let valorParcelas = valorDoProduto / quantidadeDoParcelamento;
+
+ let quantidadeDeParcelasPagas = valorPago / valorParcelas;
+
+ let restaParcela = quantidadeDoParcelamento - quantidadeDeParcelasPagas 
+
+ console.log(`Restam ${restaParcela} parcelas de R$${valorParcelas/100}`)
+
+
+
+
+
+
+
+
+
+
+
