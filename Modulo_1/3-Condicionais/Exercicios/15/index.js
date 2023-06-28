29baf9bd15f9705d05941098469a5167fab299fb
joaolotjr <joaolotjr@gmail.com>
2023-06-27 22:08:28 -0300

    Commit Exercise 15

--- Modulo_1/3-Condicionais/Exercicios/15/index.js
+++ Modulo_1/3-Condicionais/Exercicios/15/index.js
@@ -2,3 +2,9 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+if(apelido) console.log(`${apelido}`);
+
+else if(primeiroNome && sobrenome) console.log(`${primeiroNome} ${sobrenome}`);
+
+else if(primeiroNome) console.log(`${primeiroNome}`);
+
