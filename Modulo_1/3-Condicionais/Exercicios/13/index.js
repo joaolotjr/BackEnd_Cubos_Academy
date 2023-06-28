148bdba3f06a184510613210767630af19a1087d
joaolotjr <joaolotjr@gmail.com>
2023-06-27 21:45:29 -0300

    Commit Exercise 13

--- Modulo_1/3-Condicionais/Exercicios/13/index.js
+++ Modulo_1/3-Condicionais/Exercicios/13/index.js
@@ -2,5 +2,24 @@
 const tipoDePagamento = "credito";
 
 //valor da mercadoria (centavos)
-const valorDoProduto = 13000;
+const valorDoProduto = 130_00;
+
+let valorFinal = 0;
+
+if (tipoDePagamento === "credito") {
+
+    valorFinal = valorDoProduto - (valorDoProduto * 0.05)
+}
+
+else if (tipoDePagamento === "cheque") {
+
+    valorFinal = valorDoProduto - (valorDoProduto * 0.03)
+}
+
+else if (tipoDePagamento === "debito" || tipoDePagamento === "dinheiro") {
+
+    valorFinal = valorDoProduto - (valorDoProduto * 0.1)
+}
+
+console.log(`Valor a ser pago: R$ ${(valorFinal/100).toFixed(2)}`)
 
