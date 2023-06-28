b74fffe6e1a6a66e35a8af9dca0ec741ec97f8e1
joaolotjr <joaolotjr@gmail.com>
2023-06-27 21:29:42 -0300

    Commit Exercise 12 A

--- Modulo_1/3-Condicionais/Exercicios/12/index.js
+++ Modulo_1/3-Condicionais/Exercicios/12/index.js
@@ -1,1 +1,9 @@
 const diaDaSemana = 3;
+
+if (diaDaSemana === 1) console.log(`Segunda Feira`);
+else if (diaDaSemana === 2) console.log(`Terça Feira`);
+else if (diaDaSemana === 3) console.log(`Quarta Feira`);
+else if (diaDaSemana === 4) console.log(`Quinta Feira`);
+else if (diaDaSemana === 5) console.log(`Sexta Feira`);
+else if (diaDaSemana === 6) console.log(`Sabado`);
+else if (diaDaSemana === 7) console.log(`Domingo`);
