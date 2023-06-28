f5656eeea10ccbc5cfc31cd144bb09922ae050d4
joaolotjr <joaolotjr@gmail.com>
2023-06-28 19:33:22 -0300

    1 - Example Array

---
+++ Modulo_1/4-Arrays-Loops/ex1_Array.js
@@ -0,0 +1,23 @@
+const paises = ['Brasil', 'Argentina', 'Canada', 'EUA', 'Holanda']
+
+console.log(paises);
+
+paises.push('França');
+console.log(paises);
+
+paises.pop();
+console.log(paises);
+
+paises.unshift('Paraguai');
+console.log(paises);
+
+paises.shift();
+console.log(paises);
+
+
+console.log(paises.length);
+console.log(paises[paises.length - 1]);
+
+console.log(paises[1]);
+
+console.log(paises[2]);
