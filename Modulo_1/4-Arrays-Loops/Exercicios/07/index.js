49c6812c0a21f977acd68c04cc9253d6da43cf5e
joaolotjr <joaolotjr@gmail.com>
2023-06-29 20:41:51 -0300

    Commit Exercise 7

--- Modulo_1/4-Arrays-Loops/Exercicios/07/index.js
+++ Modulo_1/4-Arrays-Loops/Exercicios/07/index.js
@@ -1,1 +1,16 @@
-const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomes = ["Ana", "Joana", "Carlos", "amanda", "Lucas","Aline"];
+
+let TemA = [];
+
+for(i=0; i< nomes.length; i++){
+    for(encontraA of nomes[i]){        
+        if(encontraA =="A" || encontraA =="a"){
+           TemA.push(nomes[i]);
+           break;
+
+        }
+        break;
+    }
+}
+
+console.log(TemA)
