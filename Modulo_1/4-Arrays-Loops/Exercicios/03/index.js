6e49ab31c8fb2f43efffa5888a858bb34d5d304d
joaolotjr <joaolotjr@gmail.com>
2023-06-29 20:15:19 -0300

    Commit Exercise 3

--- Modulo_1/4-Arrays-Loops/Exercicios/03/index.js
+++ Modulo_1/4-Arrays-Loops/Exercicios/03/index.js
@@ -1,2 +1,15 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+let cont = 0;
+
+for(let i = 0; i<numeros.length; i++){
+
+    if(numeros[i] == 10){
+        console.log(i);
+        cont++;
+    }   
+}
+
+if(cont == 0){
+    console.log(-1)
+}
