34e0138d8af9b575a06e0f65af9dbb2b66d99e02
joaolotjr <joaolotjr@gmail.com>
2023-06-30 14:00:10 -0300

    Commit Exercise 12

--- Modulo_1/4-Arrays-Loops/Exercicios/12/index.js
+++ Modulo_1/4-Arrays-Loops/Exercicios/12/index.js
@@ -1,3 +1,12 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+const filaDeDentro = ["Jose", "Joao"];
+const filaDeFora = ["Joana", "Roberta"];
 
+while (filaDeDentro.length <= 4 ){
+
+    if( filaDeFora.length == 0 ){break}
+
+    else {filaDeDentro.push(filaDeFora.shift())}
+}
+
+console.log(filaDeDentro);
+console.log(filaDeFora);
