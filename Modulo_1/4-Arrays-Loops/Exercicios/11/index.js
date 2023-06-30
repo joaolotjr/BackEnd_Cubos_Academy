415bc3593528bb24fbbb1723b80757f41c583793
joaolotjr <joaolotjr@gmail.com>
2023-06-30 13:43:02 -0300

    Commit Exercise 11

--- Modulo_1/4-Arrays-Loops/Exercicios/11/index.js
+++ Modulo_1/4-Arrays-Loops/Exercicios/11/index.js
@@ -1,2 +1,13 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 10, 13, 17, 21, 26, 34, 100, 118, 245];
+
+let array = [];
+
+for(num of original){
+    if((num >=10 && num <=20) || num > 100 ){
+
+        array.push(num)
+    }
+}
+
+console.log(array)
 
