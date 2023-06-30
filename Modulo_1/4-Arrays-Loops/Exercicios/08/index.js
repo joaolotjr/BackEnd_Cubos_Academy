584c067df8b8941ffbd8805c7d0c5c4079f32514
joaolotjr <joaolotjr@gmail.com>
2023-06-30 13:25:55 -0300

    Commit Exercise 8

--- Modulo_1/4-Arrays-Loops/Exercicios/08/index.js
+++ Modulo_1/4-Arrays-Loops/Exercicios/08/index.js
@@ -1,1 +1,15 @@
 const numeros = [3, 24, 1, 8, 11, 7, 15];
+
+let menor = Number.POSITIVE_INFINITY;
+
+for(num of numeros){
+
+    if(num < menor){
+        menor = num;
+    }
+}
+
+console.log(menor);
+
+
+
