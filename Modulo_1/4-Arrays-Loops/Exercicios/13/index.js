8c07b6fbcdc16cc937f40d5be0777651691eb406
joaolotjr <joaolotjr@gmail.com>
2023-06-30 14:06:23 -0300

    Commit Exercise 13

--- Modulo_1/4-Arrays-Loops/Exercicios/13/index.js
+++ Modulo_1/4-Arrays-Loops/Exercicios/13/index.js
@@ -1,2 +1,24 @@
-const disjuntores = [false, false, true, false, false, true, false, false];
+const disjuntores = [
+  false,
+  true,
+  true,
+  false,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+
+for(i = 0; i < disjuntores.length ; i++ ){
+
+    if(disjuntores[i]){
+        console.log(i)
+    }
+}
+
+
+
 
