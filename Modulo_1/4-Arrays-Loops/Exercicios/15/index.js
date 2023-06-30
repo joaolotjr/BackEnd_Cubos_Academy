80aadc533d319af71584f17afb891105a4e1c00c
joaolotjr <joaolotjr@gmail.com>
2023-06-30 14:27:03 -0300

    Commit Exercise 15

--- Modulo_1/4-Arrays-Loops/Exercicios/15/index.js
+++ Modulo_1/4-Arrays-Loops/Exercicios/15/index.js
@@ -2,3 +2,48 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+// usando fo of
+/* 
+for(num of original){
+
+    if (num % 2 == 0){
+        pares.push(num);
+    }
+    else if(num % 2 != 0){
+        impares.push(num);
+    }
+}
+*/
+
+// Usando while
+
+
+/*
+let i = 0;
+while(i < original.length)
+{
+    if (original[i] % 2 == 0){
+        pares.push(original[i]);
+    }
+    else if(original[i] % 2 != 0){
+        impares.push(original[i]);
+    }
+    i++;
+}
+*/
+
+//usando for de C
+
+for(i = 0; i < original.length; i++){
+
+    if (original[i] % 2 == 0){
+        pares.push(original[i]);
+    }
+    else if(original[i] % 2 != 0){
+        impares.push(original[i]);
+    }
+}
+
+
+console.log(pares)
+console.log(impares)
