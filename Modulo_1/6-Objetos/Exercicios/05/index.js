1aca0af2f02648a5e030cdff66f59062880e8205
joaolotjr <joaolotjr@gmail.com>
2023-07-04 17:51:44 -0300

    Commit Exercise 5

---
+++ Modulo_1/6-Objetos/Exercicios/05/index.js
@@ -0,0 +1,29 @@
+const aulas = [
+{
+    identificador: 1,
+    nome_da_aula: "Introdução a programação"
+},
+{
+    identificador: 2,
+    nome_da_aula: "Variáveis"
+},
+{
+    identificador: 3,
+    nome_da_aula: "Condicionais"
+},
+{
+    identificador: 4,
+    nome_da_aula: "Arrays"
+}]
+
+
+
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas,
+}
+
+console.log(curso)
+
+
