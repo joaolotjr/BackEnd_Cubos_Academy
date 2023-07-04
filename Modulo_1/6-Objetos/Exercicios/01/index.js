21d8e3a29a64d40705bf0202ede1817407745993
joaolotjr <joaolotjr@gmail.com>
2023-07-04 16:57:38 -0300

    Commit Exercise 1

---
+++ Modulo_1/6-Objetos/Exercicios/01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Rua professora barbara",
+    numero: "406",
+    complemento: "Casa",
+    bairro: "burita",
+    cep: "30492-025",
+    cidade: "Belo Monte",
+    estado: "São Paulo",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "Joao",
+    email: "joao@gmail.com",
+    telefone: "11995940495",
+    data_nascimento: "20/02/1995",
+    endereco
+}
+
+console.log(usuario)
