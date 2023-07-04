de39cd48d6a080885c1d0017c208b3e25f854ee9
joaolotjr <joaolotjr@gmail.com>
2023-07-04 17:05:26 -0300

    Commit Exercise 2

---
+++ Modulo_1/6-Objetos/Exercicios/02/index.js
@@ -0,0 +1,28 @@
+const carros = [{
+    marca: "Nissan",
+    modelo: "Leaf",
+    ano: 2023,
+    cor: "Chumbo",
+    quantidades_portas: 4,
+    automatico: true
+},
+{
+    marca: "chevrolet",
+    modelo: "S10",
+    ano: 2023,
+    cor: "preto",
+    quantidades_portas: 4,
+    automatico: true
+},
+
+{
+    marca: "Nissan",
+    modelo: "frontier",
+    ano: 2022,
+    cor: "Branco",
+    quantidades_portas: 4,
+    automatico: true
+}]
+
+
+console.log(carros)
