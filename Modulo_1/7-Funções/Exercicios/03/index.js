8b3a58878a06f4b17366d21d5b0ecfe564b27030
joaolotjr <joaolotjr@gmail.com>
2023-07-12 02:20:23 -0300

    Commit Exercise 3

--- Modulo_1/7-Funções/Exercicios/03/index.js
+++ Modulo_1/7-Funções/Exercicios/03/index.js
@@ -15,18 +15,10 @@ const carrinho = {
         }
     ],
     imprimirResumo: function () {
-        let itens = 0, total = 0;
-
-    for (i = 0 ; i < this.produtos.length ; i++ ){
-
-        itens += this.produtos[i].qtd
-        total += (this.produtos[i].qtd * this.produtos[i].precoUnit);
-
-    }
-
+        
     console.log(`Cliente: ${this.nomeDoCliente}`)
-    console.log(`Total de itens: ${itens}`)
-    console.log(`Total a pagar: R$ ${(total/100).toFixed(2)}`)
+    console.log(`Total de itens: ${this.calcularTotalDeItens ()}`)
+    console.log(`Total a pagar: R$ ${(this.calcularTotalAPagar () /100).toFixed(2)}`)
 
     },
 
@@ -47,19 +39,84 @@ const carrinho = {
     },
     imprimirDetalhes: function () {
 
-        console.log(`Cliente: ${this.nomeDoCliente } /n`)
+        console.log(`Cliente: ${this.nomeDoCliente }`)
+        console.log(``)
 
         for (i = 0 ; i < this.produtos.length ; i++ ){
-            if(this.produtos[i].id == produto.id){
-                temNoCarrinho = true
-                this.produtos[i].qtd += produto.qtd;
-            }                   
+            console.log(`Item ${this.produtos[i].id} - ${this.produtos[i].nome} - ${this.produtos[i].qtd} und - R$ ${((this.produtos[i].precoUnit * this.produtos[i].qtd)/100).toFixed(2)}`)                
         }
 
+        console.log(``)
+        console.log(`Total de itens: ${this.calcularTotalDeItens ()} itens`)
+        console.log(`Total a pagar: ${(this.calcularTotalAPagar() / 100).toFixed(2) }`)
+
+    }, 
+    calcularTotalDeItens: function (){
+
+        let itens = 0;
+
+        for (i = 0 ; i < this.produtos.length ; i++ ){
+
+            itens += this.produtos[i].qtd
+            
+        }
+        return itens;
+
+    },
+    calcularTotalAPagar: function (){
+
+        let total = 0;
+
+        for (i = 0 ; i < this.produtos.length ; i++ ){
+
+            total += (this.produtos[i].qtd * this.produtos[i].precoUnit);
+
+        }
+        return total;
+
+    },
+    calcularDesconto: function(){
+        let desconto = 0, des1 = Number.POSITIVE_INFINITY, des2 = 0;
+
+        if(this.calcularTotalDeItens() > 4 ){
+            for(i = 0 ; i < this.produtos.length - 1 ; i++ ){
+                if(this.produtos[i].precoUnit <= this.produtos[i + 1].precoUnit && des1 > this.produtos[i].precoUnit){
+                    des1 = this.produtos[i].precoUnit
+                }
+                else if (this.produtos[i].precoUnit > this.produtos[i + 1].precoUnit && des1 > this.produtos[i + 1].precoUnit){
+                    des1 = this.produtos[i + 1].precoUnit;
+                }
+                else if(this.produtos.length == 1 ){
+                    des1 = this.produtos[i].precoUnit
+                }
+            }
+        }
+
+        if(this.calcularTotalAPagar() > 100_00 ){
+            des2 = this.calcularTotalAPagar() * 0.1;
+        }
+
+        if(des1 >= des2){
+            desconto = des1;
+        }
+        else if( des1 < des2 ){
+            desconto = des2;
+        }
+
+        return desconto;
     }
 
 
 }
+/*
+carrinho.imprimirDetalhes();
+
+carrinho.imprimirResumo()
+*/
+
+
+
+
 
 
 const novaBermuda = {
@@ -72,6 +129,8 @@ const novaBermuda = {
 carrinho.addProduto(novaBermuda);
 carrinho.imprimirResumo();
 
+console.log(`desconto de ${(carrinho.calcularDesconto()/100).toFixed(2)}`);
+console.log(``)
 const novoTenis = {
     id: 3,
     nome: "Tenis",
@@ -83,7 +142,7 @@ carrinho.addProduto(novoTenis);
 carrinho.imprimirResumo();
 
 
-
+console.log(`desconto de ${(carrinho.calcularDesconto()/100).toFixed(2)}`);
 
 
 
