e2743c3a584d359b9bcc89f16e1e2fcadc06b03a
joaolotjr <joaolotjr@gmail.com>
2023-07-11 23:20:30 -0300

    Commit Exercise 3 - until  F

--- Modulo_1/7-Funções/Exercicios/03/index.js
+++ Modulo_1/7-Funções/Exercicios/03/index.js
@@ -5,13 +5,134 @@ const carrinho = {
             id: 1,
             nome: "Camisa",
             qtd: 3,
-            precoUnit: 3000
+            precoUnit: 30_00
         },
         {
             id: 2,
             nome: "Bermuda",
             qtd: 2,
-            precoUnit: 5000
+            precoUnit: 50_00
         }
-    ]
+    ],
+    imprimirResumo: function () {
+        let itens = 0, total = 0;
+
+    for (i = 0 ; i < this.produtos.length ; i++ ){
+
+        itens += this.produtos[i].qtd
+        total += (this.produtos[i].qtd * this.produtos[i].precoUnit);
+
+    }
+
+    console.log(`Cliente: ${this.nomeDoCliente}`)
+    console.log(`Total de itens: ${itens}`)
+    console.log(`Total a pagar: R$ ${(total/100).toFixed(2)}`)
+
+    },
+
+    addProduto: function (produto) {
+        let temNoCarrinho = false;
+
+        for (i = 0 ; i < this.produtos.length ; i++ ){
+            if(this.produtos[i].id == produto.id){
+                temNoCarrinho = true
+                this.produtos[i].qtd += produto.qtd;
+            }                   
+        }
+
+        if(!temNoCarrinho) {
+            this.produtos.push(produto);
+        }
+        
+    },
+    imprimirDetalhes: function () {
+
+        console.log(`Cliente: ${this.nomeDoCliente } /n`)
+
+        for (i = 0 ; i < this.produtos.length ; i++ ){
+            if(this.produtos[i].id == produto.id){
+                temNoCarrinho = true
+                this.produtos[i].qtd += produto.qtd;
+            }                   
+        }
+
+    }
+
+
 }
+
+
+const novaBermuda = {
+    id: 2,
+    nome: "Bermuda",
+    qtd: 3,
+    precoUnit: 5000
+}
+
+carrinho.addProduto(novaBermuda);
+carrinho.imprimirResumo();
+
+const novoTenis = {
+    id: 3,
+    nome: "Tenis",
+    qtd: 1,
+    precoUnit: 10000
+}
+
+carrinho.addProduto(novoTenis);
+carrinho.imprimirResumo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function imprimirResumoDoCarrinho (carrinho) {
+
+    let itens = 0, total = 0;
+
+    for (i = 0 ; i < carrinho.produtos.length ; i++ ){
+
+        itens += carrinho.produtos[i].qtd
+        total += (carrinho.produtos[i].qtd * carrinho.produtos[i].precoUnit);
+
+    }
+
+    console.log(`Cliente: ${carrinho.nomeDoCliente}`)
+    console.log(`Total de itens: ${itens}`)
+    console.log(`Total a pagar: R$ ${(total/100).toFixed(2)}`)
+
+}
+
+function addProdutoAoCarrinho (carrinho, produto){
+
+    let temNoCarrinho = false;
+
+    for (i = 0 ; i < carrinho.produtos.length ; i++ ){
+        if(carrinho.produtos[i].id == produto.id){
+            temNoCarrinho = true
+            carrinho.produtos[i].qtd += produto.qtd;
+        }                   
+    }
+
+    if(!temNoCarrinho) {
+
+        carrinho.produtos.push(produto);
+
+    }
+    
+}
+
+
+*/
+
