16704c8267d4efa87b51568e9cf063bb3ae98b96
joaolotjr <joaolotjr@gmail.com>
2023-07-13 14:27:50 -0300

    Commit Exercise 7

--- Modulo_1/8-String Methods/Exercicios/07/index.js
+++ Modulo_1/8-String Methods/Exercicios/07/index.js
@@ -1,1 +1,10 @@
-const email = "aluno@cubos.academy";
+const email = ".messias@cubos";
+
+
+if(email.includes("@") && email.includes(".") && email.indexOf(".") != 0 && email.lastIndexOf(".") != (email.length -1)){
+
+    console.log(`E-mail válido`)
+}
+else {
+    console.log(`E-mail inválido`)
+}
