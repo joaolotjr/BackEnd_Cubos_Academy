e75b8ba66b22433f16bc94d60e28a3a19eb1da66
joaolotjr <joaolotjr@gmail.com>
2023-07-13 01:42:32 -0300

    Commit Exercise 1

--- Modulo_1/8-String Methods/Exercicios/01/index.js
+++ Modulo_1/8-String Methods/Exercicios/01/index.js
@@ -1,1 +1,10 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse covid é muito perigoso!";
+
+
+const comentarioSeparado = comentario.toUpperCase().split(" ");
+
+if(comentarioSeparado.includes("COVID") || comentarioSeparado.includes("PANDEMIA")){
+    console.log(`Comentário bloqueado por conter palavras proibidas`)
+}
+else{console.log(`Comentário autorizado`)}
+
