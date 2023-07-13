68c0f711812e75019b2c75a4b7a0ef321a9b3674
joaolotjr <joaolotjr@gmail.com>
2023-07-13 14:49:30 -0300

    Commit Exercise 10

--- Modulo_1/8-String Methods/Exercicios/10/index.js
+++ Modulo_1/8-String Methods/Exercicios/10/index.js
@@ -1,1 +1,14 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.png';
+
+let indexponto = nomeArquivo.lastIndexOf(".");
+
+if(nomeArquivo.includes('jpg', indexponto)  || nomeArquivo.includes('jpeg', indexponto) || nomeArquivo.includes('gif', indexponto) || nomeArquivo.includes('png', indexponto)){
+    console.log(`Arquivo válido`)
+
+}
+else{
+    console.log(`Arquivo inválido`)
+}
+
+
+
