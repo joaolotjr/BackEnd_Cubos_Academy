f5a110781fb07d52b7d2913d808c9717726ef34e
joaolotjr <joaolotjr@gmail.com>
2023-07-12 23:34:51 -0300

    1 - Example String Methods

--- Modulo_1/8-String Methods/ex1_String_Methods.js
+++ Modulo_1/8-String Methods/ex1_String_Methods.js
@@ -1,16 +1,3 @@
 const string = "97.50";
 
-
-
 console.log(string.replace('.',','))
-
-
-
-for(i = 0; i < string.length; i++){
-
-    if(string[i] === '.'){
-        string[i] += ',';
-    }    
-}
-
-console.log(string)
