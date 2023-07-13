41680cf22869b6433aab9fea81419c5640594680
joaolotjr <joaolotjr@gmail.com>
2023-07-13 14:34:25 -0300

    Commit Exercise 8

--- Modulo_1/8-String Methods/Exercicios/08/index.js
+++ Modulo_1/8-String Methods/Exercicios/08/index.js
@@ -1,1 +1,15 @@
 const cpf = "011.022.033-44";
+
+let cpfSemPonto = cpf.replace(".", "");
+cpfSemPonto = cpfSemPonto.replace(".", "");
+cpfSemPonto = cpfSemPonto.replace("-", "");
+
+console.log(cpfSemPonto)
+/*
+for(i=0; i<cpf.length;i++){
+    if()
+
+}
+*/
+
+
