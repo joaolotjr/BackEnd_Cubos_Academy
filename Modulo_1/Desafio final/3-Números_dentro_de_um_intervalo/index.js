5258f99f0e7fe7b549b6907c57c04a7eecd001b0
joaolotjr <joaolotjr@gmail.com>
2023-07-13 17:20:07 -0300

    3 - Completed exercise

---
+++ Modulo_1/Desafio final/3-Números_dentro_de_um_intervalo/index.js
@@ -0,0 +1,11 @@
+const numero = 16, limiteInferior = 5, limiteSuperior = 20;
+
+// copiar apartir daqui
+
+if(numero >= limiteInferior && numero <= limiteSuperior){
+    console.log(`PERTENCE`)
+}
+else{
+    console.log(`NAO PERTENCE`)
+}
+
