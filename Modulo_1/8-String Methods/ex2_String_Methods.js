ab4bfd95deb2f928f2584c1ae0a5c56229073457
joaolotjr <joaolotjr@gmail.com>
2023-07-12 23:57:50 -0300

    2 - Example String Methods

--- Modulo_1/8-String Methods/ex2_String_Methods.js
+++ Modulo_1/8-String Methods/ex2_String_Methods.js
@@ -1,3 +1,27 @@
 
+const num = "1,257,257,258"
+
+//jeito menor
+console.log(num.replace(/,/g,'.'))
+
+//jeito usando for
+function alteraVirgulaPPonto (string){
+
+    let SemVirgula = "";
+
+
+for (i = 0; i< string.length; i++){
+    if(string[i] === ","){
+        SemVirgula += ".";
+    }
+    else{
+        SemVirgula += string[i]    
+    }
+}
+
+return SemVirgula
+}
+
+console.log(alteraVirgulaPPonto (num));
 
 
