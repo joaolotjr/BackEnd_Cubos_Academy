42ace45e6eb70740c5fe07fa0fb6faf110b3f2b9
joaolotjr <joaolotjr@gmail.com>
2023-07-13 02:25:15 -0300

    Commit Exercise 4

--- Modulo_1/8-String Methods/Exercicios/04/index.js
+++ Modulo_1/8-String Methods/Exercicios/04/index.js
@@ -1,3 +1,42 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+//---------Identificador-----------
+//
+identificador = identificador.padStart(6, "0")
+console.log(identificador)
+
+//--------------Nome--------------
+
+function primiraMaiuscula (nomeUnico) { // coloca a string com a primeira letra maiuscula
+    const maiuscula = nomeUnico[0].toUpperCase() + nomeUnico.substr(1);
+    return maiuscula;
+}
+
+function retornaComInicialMaiuscula (nome){
+
+    let nomesSeparados = nome.split(" ");
+    let nomesJuntos = []; //,
+    for(i = 0; i < nomesSeparados.length; i++ ){
+        nomesJuntos.push(primiraMaiuscula(nomesSeparados[i]))//=  primiraMaiuscula(nomesSeparados)
+    }
+    let nomesjuntosMaiuscula = nomesJuntos.join(" ");
+    //console.log(nomesjuntosMaiuscula)
+    return nomesjuntosMaiuscula
+}
+nome = retornaComInicialMaiuscula(nome);
+console.log(nome)
+
+//----------------------------------------
+//--------------Email--------------
+
+email = email.replace(/ /g, "");
+
+console.log(email)
+
+
+
+
+
+
