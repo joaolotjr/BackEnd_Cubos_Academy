e9b7ed4d943bc6a48c89133ee8a80fef978cf7f6
joaolotjr <joaolotjr@gmail.com>
2023-07-13 03:54:42 -0300

    Commit Exercise 5

--- Modulo_1/8-String Methods/Exercicios/05/index.js
+++ Modulo_1/8-String Methods/Exercicios/05/index.js
@@ -1,1 +1,16 @@
 const numeroCartao = '1111222233334444';
+
+let dadostampados = "" ;
+
+
+for(i = 0; i < numeroCartao.length; i++){
+
+    if(i >= 4 && i <= 11){
+        dadostampados += "*";
+    }
+    else {
+        dadostampados += numeroCartao[i];
+    }
+}
+
+console.log(dadostampados);
