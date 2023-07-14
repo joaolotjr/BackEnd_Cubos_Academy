1cc1d1c84b079e1691a40579b2108cafac778e95
joaolotjr <joaolotjr@gmail.com>
2023-07-14 01:25:56 -0300

    5 - Completed exercise

---
+++ Modulo_1/Desafio final/5-Truco/index.js
@@ -0,0 +1,24 @@
+const carta = "Q";
+
+// copiar apartir daqui
+
+
+if(carta == "Q"){
+    console.log(`J`)
+}
+else if (carta == "J"){
+    console.log(`K`)
+}
+else if (carta == "K"){
+    console.log(`A`)
+}
+else if (carta == "A"){
+    console.log(`2`)
+}
+else if (carta == "2"){
+    console.log(`3`)
+}
+else if (carta == "3"){
+    console.log(`Q`)
+}
+
