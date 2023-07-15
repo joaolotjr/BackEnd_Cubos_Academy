48742a56ec5b6cf723115ff4af3be5c7f2d8e6de
joaolotjr <joaolotjr@gmail.com>
2023-07-14 23:55:56 -0300

    10 - Completed exercise

--- Modulo_1/Desafio final/10-Zerinho_ou_Um/index.js
+++ Modulo_1/Desafio final/10-Zerinho_ou_Um/index.js
@@ -35,9 +35,31 @@ const jogadores = [
 
   // copiar apartir daqui
 
+   let jogadores0 = "", jogadores1 = "";
+   let cont0=0,cont1=0;
+   
 
+  for(i = 0; i < jogadores.length; i++){
 
+    if(jogadores[i].jogada == 0){
+      jogadores0 = jogadores[i].nome; 
+      cont0++;
+      //console.log(jogadores0)          
+    }
+    if(jogadores[i].jogada == 1){
+      jogadores1 = jogadores[i].nome; 
+      cont1++;
+      //console.log(jogadores1)
 
+    }
+    
+  }
 
-
-  if(sorteado == )
+ if(cont0 == 1){
+  console.log(jogadores0)
+  cont0++
+ }
+ else if(cont1 == 1){
+  console.log(jogadores1)
+ }
+ else{console.log(`NINGUEM`)}
