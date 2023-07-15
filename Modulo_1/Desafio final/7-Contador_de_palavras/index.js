6de900e7cd22cef9aa80dabee3f6e55c8bf1e6fc
joaolotjr <joaolotjr@gmail.com>
2023-07-15 01:14:57 -0300

    07 - Completed exercise

--- Modulo_1/Desafio final/7-Contador_de_palavras/index.js
+++ Modulo_1/Desafio final/7-Contador_de_palavras/index.js
@@ -1,45 +1,11 @@
-const texto = "  . ";
+const texto = " ";
 
-// copiar apartir daqui
+// copiar a partir daqui
 
+let dados = texto.trim().replace(/\s{2,}/g, " ").split(" "); //regex desgraamado!
 
-let listaComUmEspacoSo = "", leng = (texto.length - 1)
-
-for(i=0 ; i<texto.length ; i++){
-    if(texto[i] == " " && texto[i+1] == " "){
-        i++;
-    }
-    else if(i == 0 && texto[i] == " " ){
-                
-    }
-    else if(i == leng && texto[i] == " "){
-        
-    }
-    else{
-        listaComUmEspacoSo += texto[i];
-    }
-}
-
-//console.log(listaComUmEspacoSo)
-
-let contador = 0, NumeroDePalavras = 0
-
-for(i=0; i<listaComUmEspacoSo.length; i++){
-    if(listaComUmEspacoSo[i] == " "){
-        contador++
-    }    
-}
-
-if(listaComUmEspacoSo.length > 0 && contador == 0){
-    console.log(1)
-}
-else if(listaComUmEspacoSo.length === 0){
-    console.log(0)
-}
-else if(contador > 0){
-    console.log(`${contador + 1}`)
-}
-else{console.log(contador)}
+if(texto.length === 0 || texto == " "){console.log(0)}
+else{ console.log(dados.length)}
 
 
 
