d18023ff8ab4cd4bce9c25eef349551dba54ef27
joaolotjr <joaolotjr@gmail.com>
2023-07-15 00:06:50 -0300

    12 - Completed exercise

--- Modulo_1/Desafio final/12-cAPS_lOCK_oN/index.js
+++ Modulo_1/Desafio final/12-cAPS_lOCK_oN/index.js
@@ -1,12 +1,24 @@
-const input = 'cAaFArAr';
-
-// copiar apartir daqui
+const input = 'aBAaAXI';
 
 //1º - palavra toda maiuscula -> retornar toda minuscula
 //2º - Primeira minuscula e resto maiuscula -> inverter todas as letras
 //3º else - saira igual a entrada
 
 
+// copiar apartir daqui
+
+if(input == input.toUpperCase()){
+    console.log(input.toLocaleLowerCase())
+}
+
+else if(input[0] == input[0].toLowerCase() && input.slice(1) == input.slice(1).toUpperCase()){ 
+    console.log(`${input[0].toUpperCase()}${input.slice(1).toLowerCase()}`)
+}
+
+else{console.log(input)}
+
+/*
+lógica com monitor:
 
 if(input[0] == input[0].toUpperCase()){
     console.log(`A primeira letra maiuscula`)
@@ -15,3 +27,4 @@ if(input.slice(1) == input.slice(1).toUpperCase()){
     console.log(`O resto da palavra é MAIUSCULA`)
 }
 
+*/
