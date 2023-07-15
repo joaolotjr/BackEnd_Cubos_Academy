4af905e958b71bc68de840cae5926e99e258a991
joaolotjr <joaolotjr@gmail.com>
2023-07-14 23:06:34 -0300

    10-Cofre_Bugadão

---
+++ Modulo_1/Desafio final/10-Zerinho_ou_Um/index.js
@@ -0,0 +1,43 @@
+const jogadores = [
+    {
+      "nome": "Herman",
+      "jogada": 1
+    },
+    {
+      "nome": "Rhodes",
+      "jogada": 0
+    },
+    {
+      "nome": "Beach",
+      "jogada": 0
+    },
+    {
+      "nome": "Laurel",
+      "jogada": 0
+    },
+    {
+      "nome": "Beatrice",
+      "jogada": 0
+    },
+    {
+      "nome": "Alison",
+      "jogada": 0
+    },
+    {
+      "nome": "Saundra",
+      "jogada": 0
+    },
+    {
+      "nome": "Klein",
+      "jogada": 0
+    }
+  ]
+
+  // copiar apartir daqui
+
+
+
+
+
+
+  if(sorteado == )
