455901e71b10ff3026aeb94f0ad1944cae21c4b8
joaolotjr <joaolotjr@gmail.com>
2023-07-27 21:47:17 -0300

    my first server

---
+++ Modulo_2/1-Primeiro_Servidor/array.js
@@ -0,0 +1,9 @@
+const arrayNumeros = [1, 2, 4, 4, 6, 7, 1];
+
+const arrayLetras = ['a', 'a', 's', 'c'];
+
+module.exports = {
+    arrayNumeros,// igual a arrayNumeros: arrayNumeros,
+    arrayLetras// igual a arrayLetras: arrayLetras
+}
+
--- Modulo_2/1-Primeiro_Servidor/index.js
+++ Modulo_2/1-Primeiro_Servidor/index.js
@@ -1,7 +1,19 @@
 const { uniq } = require('lodash');
+const {arrayLetras, arrayNumeros} = require('./array')
 
-const array = [1, 2, 4, 4, 6, 7, 1];
+console.log(arrayLetras);
+console.log(arrayNumeros)
 
+const arrayNumeroUnico = uniq(arrayNumeros)
+const arrayLetrasUnica = uniq(arrayLetras)
+
+console.log(arrayNumeroUnico);
+console.log(arrayLetrasUnica);
+/*
 const arrayUnico = uniq(array);
 
+
+
 console.log(arrayUnico);
+
+*/
