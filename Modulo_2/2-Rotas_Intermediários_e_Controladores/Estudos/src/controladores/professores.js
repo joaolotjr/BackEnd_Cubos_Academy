2f5c271756a84e9f0880c5fe1800412c82c69b1a
joaolotjr <joaolotjr@gmail.com>
2023-07-29 23:03:11 -0300

    file organization

---
+++ Modulo_2/2-Rotas_Intermediários_e_Controladores/Estudos/src/controladores/professores.js
@@ -0,0 +1,27 @@
+const professores = require ('../bancodedados');
+
+const filtrarProfessores = (req, res) =>{
+    const {Stack} = req.query
+    let resultado = professores;
+
+    if(Stack){ 
+        resultado = professores.filter((professor) => {
+            return professor.Stack === Stack;
+        })
+
+    }
+    
+    res.send(resultado)
+}
+
+const encontraProfessor = (req, res) =>{
+    const professoresEncontrado = professores.find((professor) => {
+        return professor.id === Number(req.params.id);
+    })
+    res.send(professoresEncontrado)
+}
+
+module.exports = {
+    filtrarProfessores,
+    encontraProfessor
+}
