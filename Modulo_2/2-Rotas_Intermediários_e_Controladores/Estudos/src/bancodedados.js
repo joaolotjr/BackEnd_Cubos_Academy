c9e60ff45d6bbdb37865fb81fc3d4d01bca77812
joaolotjr <joaolotjr@gmail.com>
2023-07-29 23:03:06 -0300

    file organization

---
+++ Modulo_2/2-Rotas_Intermediários_e_Controladores/Estudos/src/bancodedados.js
@@ -0,0 +1,24 @@
+const professores = [
+    {
+        id: 1,
+        nome: 'Guido',
+        Stack: 'Backend'
+    },
+    {
+        id: 2,
+        nome: 'Dani',
+        Stack: 'Frontend'
+    },
+    {
+        id: 3,
+        nome: 'Diego',
+        Stack: 'Frontend'
+    },
+    {
+        id: 4,
+        nome: 'Vidal',
+        Stack: 'Backend'
+    },
+]
+
+module.exports = professores;
--- Modulo_2/2-Rotas_Intermediários_e_Controladores/Estudos/src/index.js
+++ Modulo_2/2-Rotas_Intermediários_e_Controladores/Estudos/src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const {filtrarProfessores, encontraProfessor} = require('./controladores/professores');
+const professores = require ('./bancodedados');
 
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send(`pagina inicial 1`)
-
-
+    res.send(`Pagina inicial`)
 })
+
+app.get('/professores', filtrarProfessores)
+
+
+app.get('/professores/:id', encontraProfessor )
 
 
 app.listen(3000)
