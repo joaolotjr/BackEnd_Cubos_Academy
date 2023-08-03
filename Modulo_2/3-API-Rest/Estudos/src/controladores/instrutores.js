ea9be1cd94946efbf4ba6ff6a3ec614413daf8dc
joaolotjr <joaolotjr@gmail.com>
2023-08-03 01:50:33 -0300

    add obterInstrutor

--- Modulo_2/3-API-Rest/Estudos/src/controladores/instrutores.js
+++ Modulo_2/3-API-Rest/Estudos/src/controladores/instrutores.js
@@ -3,10 +3,22 @@ const {instrutores} = require('../bancodedados')
 
 const listarInstrutores = (req, res) => {
     return res.status(200).json(instrutores)
+}
 
+const obterInstrutor = (req, res) => {
 
+    const {id} = req.params;
+
+    const instrutor = instrutores.find((instrutor) =>{
+        return instrutor.id === Number(id);
+    })
+    if(!instrutor){
+        return res.status(404).json({ mensagem: 'Instrutor não encontrado'})
+    }
+    return res.status(200).json(instrutor)
 }
 
 module.exports = {
-    listarInstrutores
+    listarInstrutores,
+    obterInstrutor
 }
--- Modulo_2/3-API-Rest/Estudos/src/rotas.js
+++ Modulo_2/3-API-Rest/Estudos/src/rotas.js
@@ -4,5 +4,6 @@ const instrutores = require('./controladores/instrutores');
 const rotas = express();
 
 rotas.get('/instrutores', instrutores.listarInstrutores)
+rotas.get('/instrutores/:id', instrutores.obterInstrutor)
 
 module.exports = rotas;
