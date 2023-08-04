8fe68b6d1313b0e3df1ef064f135e01a01091bf4
joaolotjr <joaolotjr@gmail.com>
2023-08-04 02:21:34 -0300

    exercise with getPackageDescriptionNpm

--- Modulo_2/4-Programação_Assíncrona/Exercicio_aula_1/src/index.js
+++ Modulo_2/4-Programação_Assíncrona/Exercicio_aula_1/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { getCityFromZipcode } = require('utils-playground');
+const { getCityFromZipcode, getPackageDescriptionNpm } = require('utils-playground');
  
 const app = express();
 
 app.get('/', async (req, res) =>{
     const cidade =  getCityFromZipcode('30492025')
-    const cidade2 =  getCityFromZipcode('17054060')
+    const cidade2 =  getCityFromZipcode('35930002')
 
     const promise = await Promise.all([cidade, cidade2])
 
@@ -15,5 +15,17 @@ app.get('/', async (req, res) =>{
 
     res.send(`A cidade encontrada foi ${resposta1} e ${resposta2}`)
 })
+
+
+app.get('/pacote/:nomePacote', async (req, res) =>{
+
+    const { nomePacote } = req.params;
+
+    const descricaopacote =  await getPackageDescriptionNpm(nomePacote)
+    
+    //console.log(promise)
+    
+    return res.send(descricaopacote)
+});
 
 app.listen(3000)
