1d9222d648a1859d1ef44419ce723956b84d47db
joaolotjr <joaolotjr@gmail.com>
2023-08-04 19:15:37 -0300

    Studies - JSON parse and stringify

---
+++ Modulo_2/5-Leitura_e_Escrita_em_Arquivos/Estudos-JSON_parse_e_stringify/scripts.js
@@ -0,0 +1,18 @@
+const fs = require('fs/promises');
+
+
+(async function() {
+    const arquivoJson = await fs.readFile('teste.json')
+
+    const pessoas = JSON.parse(arquivoJson)
+
+    pessoas.push({
+        nome: 'Guido',
+        idade: 31
+    });
+
+    const arrayJson = JSON.stringify(pessoas)
+
+    //console.log(arquivoJson.toString())
+    console.log(arrayJson)
+})();
