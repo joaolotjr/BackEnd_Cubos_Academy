ae88600f62c97a1e92dba9554d3557dbb50df2e5
joaolotjr <joaolotjr@gmail.com>
2023-08-04 01:46:05 -0300

    study with asynchronous function

--- Modulo_2/4-Programação_Assíncrona/Estudo_funcoes_assincronas/scripts.js
+++ Modulo_2/4-Programação_Assíncrona/Estudo_funcoes_assincronas/scripts.js
@@ -1,12 +1,5 @@
-const {getCityFromZipcode} = require('utils-playground');
+const {getCityFromZipcode, getStateFromZipcode} = require('utils-playground');
 /*
-const cidade = getCityFromZipcode('41256250').then((cidade) => {
-    console.log(cidade)
-}).catch((erro)=>{
-    console.log(erro)
-})
-*/
-//------------------------------------------------
 const cidade = getCityFromZipcode('41256250')
 
 cidade.then((cidade) => {
@@ -16,5 +9,51 @@ cidade.then((cidade) => {
 cidade.catch((erro)=>{
     console.log(erro)
 })
+*/
+//------------------------------------------------
+/*
+const cidade = getCityFromZipcode('41256250').then((cidade) => {
+    console.log(cidade)
+    getStateFromZipcode('41256250').then((uf) => {
+        console.log(uf)
+        getStateFromZipcode('41256250').then((uf) => {
+            console.log(uf)
+            getStateFromZipcode('41256250').then((uf) => {
+                console.log(uf)
+            })
+        })
+    })
+}).catch((erro)=>{
+    console.log(erro)
+})
 
-console.log(cidade)
+
+getStateFromZipcode('41256250').then((uf) => {
+    console.log(uf)
+})
+*/
+
+//funcao anonima e uso do async/await
+/*
+(async function (){
+
+    const cidade = await getCityFromZipcode('41256250');
+    console.log(cidade)
+    const estado = await getStateFromZipcode('41256250')
+    console.log(estado)
+})()
+*/
+
+async function teste(a){
+    return '123'
+}
+
+const teste = async () =>{
+    
+}
+
+console.log(teste())
+
+
+
+
