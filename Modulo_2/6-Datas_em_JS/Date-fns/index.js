52d24a48cfc460fdaf39546432c5bdee996db2f8
joaolotjr <joaolotjr@gmail.com>
2023-08-05 18:12:35 -0300

    Date-fns - parse

--- Modulo_2/6-Datas_em_JS/Date-fns/index.js
+++ Modulo_2/6-Datas_em_JS/Date-fns/index.js
@@ -1,5 +1,12 @@
+//----------Date-fns - format
+/*
 const {format} = require('date-fns');
 
 const date = new Date(2017, 3, 12);
 
 console.log(format(date, "dd/MMM/yyyy"))
+*/
+
+//----------Date-fns - parse
+
+const result = parse('02/11/2014', 'MM/dd/yyyy', new Date())
