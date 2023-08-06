f0ebcd3085e9215aeffdb51ac02c61dd28831dbe
joaolotjr <joaolotjr@gmail.com>
2023-08-06 16:18:40 -0300

    Date-fns - add

--- Modulo_2/6-Datas_em_JS/Date-fns/index.js
+++ Modulo_2/6-Datas_em_JS/Date-fns/index.js
@@ -8,5 +8,47 @@ console.log(format(date, "dd/MMM/yyyy"))
 */
 
 //----------Date-fns - parse
-
+/*
 const result = parse('02/11/2014', 'MM/dd/yyyy', new Date())
+*/
+
+//----------Date-fns - add
+/*
+const { add } = require('date-fns');
+let date = new Date(2018, 2, 13, 12)
+
+console.log(date);
+/*console.log(add(date,{
+    seconds: 2,
+    days: 1,
+    years: 1
+    }));
+*/
+date = add(date,{
+    seconds: 2,
+    days: 1,
+    years: 1
+    });
+console.log(date);
+/*
+
+//----------Date-fns - addBusinessDay
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
