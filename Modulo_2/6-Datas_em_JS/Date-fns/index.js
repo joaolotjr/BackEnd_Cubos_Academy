259918782dabc0776d40eb005987faae3adbfcc3
joaolotjr <joaolotjr@gmail.com>
2023-08-06 20:05:49 -0300

    Studies - convert to other Timezones

--- Modulo_2/6-Datas_em_JS/Date-fns/index.js
+++ Modulo_2/6-Datas_em_JS/Date-fns/index.js
@@ -80,6 +80,28 @@ const result1 = startOfHours(new Date(2014, 8, 1, 1, 23, 45))
 console.log(result1);
 */
 
+//----------  Atenção ao converter para outras Timezones
+const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz')
+
+// Set the date to "2018-09-01T16:01:36.386Z"
+const utcDate = zonedTimeToUtc('2018-09-01 18:01:36.386', 'Europe/Berlin')
+
+// Obtain a Date instance that will render the equivalent Berlin time for the UTC date
+const date = new Date('2018-09-01T16:01:36.386Z')
+const timeZone = 'Europe/Berlin'
+const zonedDate = utcToZonedTime(date, timeZone)
+// zonedDate could be used to initialize a date picker or display the formatted local date/time
+
+// Set the output to "1.9.2018 18:01:36.386 GMT+02:00 (CEST)"
+const pattern = 'd.M.yyyy HH:mm:ss.SSS \'GMT\' XXX (z)'
+const output = format(zonedDate, pattern, { timeZone: 'Europe/Berlin' })
+
+
+const date = new Date(2021, 0, 1, 13);
+console.log(date)
+console.log(+date)
+
+console.log(date.getUTCHours())
 
 
 
