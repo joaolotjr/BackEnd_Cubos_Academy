bf344468009e9fe4b52f53405d1aef04e067cc3d
joaolotjr <joaolotjr@gmail.com>
2023-09-07 20:50:36 -0300

    init class cryptography

--- Modulo_3/7_Autenticação_e_Criptografia/Estudos/src/controladores/usuarios.js
+++ Modulo_3/7_Autenticação_e_Criptografia/Estudos/src/controladores/usuarios.js
@@ -1,13 +1,17 @@
 const pool = require('../conexao')
+const bcrypt = require('bcrypt')
 
 const cadastrarUsuario = async (req, res) => {
 
     const { nome, email, senha } = req.body
 
     try {
+        const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+
         const novoUsuario = await pool.query(
             'insert into usuarios (nome, email, senha) values ($1, $2, $3) returning *',
-            [nome, email, senha]
+            [nome, email, senhaCriptografada]
         )
         
         return res.status(201).json(novoUsuario.rows[0])
