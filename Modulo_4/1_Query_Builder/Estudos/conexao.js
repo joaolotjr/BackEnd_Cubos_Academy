8fefaf21545f3a8254e92d15d91aa6052d7cd666
joaolotjr <joaolotjr@gmail.com>
2023-09-14 13:17:17 -0300

    where helper methods

--- Modulo_4/1_Query_Builder/Estudos/conexao.js
+++ Modulo_4/1_Query_Builder/Estudos/conexao.js
@@ -2,7 +2,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
       host : 'localhost',
-      port : 3306,
+      port : 5432,
       user : 'postgres',
       password : '123456',
       database : 'knexjs'
--- Modulo_4/1_Query_Builder/Estudos/index.js
+++ Modulo_4/1_Query_Builder/Estudos/index.js
@@ -6,6 +6,24 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async(req,res) => {
+    //conexao.query ('select * from agenda where id = $1', [5])
+    //const agenda = await knex('agenda').debug();
+    //const agenda = await knex.raw('select * from agenda');
+    //const agenda = await knex('agenda').where('id', 5).debug();
+    //const agenda = await knex('agenda').where('id', '!=', 5).debug();
+    //const agenda = await knex('agenda').where( {id: 5, nome: 'ALguma coisa'} ).debug();
+    //const agenda = await knex('agenda').where( {id: 5} ).select('id', 'nome').first().debug();
+
+    //select * from agenda where email is null;
+    //const agenda = await knex('agenda').whereNull('email').debug();
+    //select * from agenda where email is not null;
+    //const agenda = await knex('agenda').whereNotNull('email').debug();
+    //select * from agenda where id between 5 end 10;
+    //const agenda = await knex('agenda').whereBetween('id',[5,10]).debug();
+    //const agenda = await knex('agenda').whereBetween('id',[5,10]).orderBy('id', 'desc').debug();
+    const agenda = await knex('agenda').distinct('email').debug();
+
+    return res.json(agenda)
 
 
 })
